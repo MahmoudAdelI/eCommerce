890053ac59ee4c572c56a1f17a5e3bc2a31e6977
Mahmoud Adel <mahmoud.adeli7888@gmail.com>
2026-03-11 02:39:42 +0200

    feat: implemented routing mechanism for SPA

--- src/app/router.js
+++ src/app/router.js
@@ -1,1 +1,37 @@
 // routing
+export const routes = {
+  "/": () => import("../pages/HomePage.js"),
+  "/products": () => import("../pages/ProductsPage.js"),
+};
+
+export const navigateTo = async (url) => {
+  history.pushState(null, "", url);
+  renderPage();
+};
+
+export const renderPage = async () => {
+  const root = document.getElementById("root");
+  root.innerHTML = `<div class="loader">Loading…</div>`;
+
+  const path = window.location.pathname;
+  const importFn = routes[path] || (() => import("../pages/NotFoundPage.js"));
+
+  const module = await importFn();
+  const pageContent = await module.default();
+
+  root.innerHTML = "";
+  root.appendChild(pageContent);
+};
+
+export const initRouter = async () => {
+  window.addEventListener("popstate", renderPage);
+
+  document.body.addEventListener("click", (e) => {
+    const link = e.target.closest("[data-link]");
+    if (link) {
+      e.preventDefault();
+      navigateTo(link.href);
+    }
+  });
+  renderPage();
+};
