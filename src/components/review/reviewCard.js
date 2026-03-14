e664b24c1421f13ea3e721927318162b4784d7d4
Mahmoud Adel <mahmoud.adeli7888@gmail.com>
2026-03-14 15:46:05 +0200

    feat: added review cards

---
+++ src/components/review/reviewCard.js
@@ -0,0 +1,17 @@
+export default function reviewCard(data) {
+  const stars = data.rating
+    ? "★".repeat(Math.round(data.rating)) +
+      "☆".repeat(5 - Math.round(data.rating))
+    : "☆☆☆☆☆";
+  return `
+    <div class="review-card">
+    <div class="review-card__stars">${stars}</div>
+      <div class="review-card__reviewer">
+        ${data.reviewerName}
+        <span class="review-card__success"
+          ><i class="fa-solid fa-circle-check"></i
+        ></span>
+      </div>
+      <div class="review-card__review">"${data.comment}"</div>
+      </div>`;
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import button from "../../components/button/button.js";
 import grid from "../../components/grid/grid.js";
 import productCard from "../../components/productCard/productCard.js";
+import reviewCard from "../../components/review/reviewCard.js";
 import Page from "../../utils/Page.js";
 import categoryGrid from "./components/categoryGrid/categoryGrid.js";
 import hero from "./components/hero/hero.js";
@@ -9,10 +10,16 @@ export default class Home extends Page {
   async render() {
     const url = "https://dummyjson.com/products";
     const res = await fetch(url);
-    const { products } = await res.json();
-    console.log(products);
+    const data = await res.json();
 
-    const data = products.map((p) =>
+    const reviewsData = data.products.flatMap((p) => {
+      return p.reviews.filter((r) => r.rating == 5);
+    });
+
+    console.log(data);
+    console.log(reviewsData);
+
+    const products = data.products.map((p) =>
       productCard({
         title: p?.title,
         img: p?.images[0],
@@ -23,23 +30,32 @@ export default class Home extends Page {
       }),
     );
 
+    const reviews = reviewsData.map((r) => reviewCard(r));
     const page = document.createElement("div");
     page.innerHTML = `
       ${hero()}
       <div class="container">
       
         <h1 class="heading separator-label">NEW ARRIVALS</h1>
-        ${grid(data.slice(0, 4))}
+        ${grid(products.slice(0, 4))}
         
         ${button("View All")}
         <div class="hr"></div>
         
         <h1 class="heading separator-label">TOP SELLING</h1>
-        ${grid(data.slice(4, 8))}
-
+        ${grid(products.slice(4, 8))}
+        
         ${button("View All")}
-
+        
         ${categoryGrid()}
+        
+        <h1 class="heading ">OUR HAPPY CUSTOMERS</h1>
+        
+        
+        
+        </div>
+        <div class="reviews-container">
+          ${reviews.slice(0, 12).join("")}
         </div>
     `;
     return page;
